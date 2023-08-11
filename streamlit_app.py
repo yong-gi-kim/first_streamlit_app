5fcecb9ea031b9b4dee34c04746d4bbc40b04e35
yong-gi-kim <powergambler@gmail.com>
2023-08-11 13:40:48 +0200

    test apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.dataframe(my_fruit_list.get('Apple'))
+streamlit.dataframe(my_fruit_list.get(1))
