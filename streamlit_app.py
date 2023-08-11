da16fe253202b6e4478d712ce79d3fcaac653b9e
yong-gi-kim <powergambler@gmail.com>
2023-08-11 13:20:48 +0200

    import pandas lib and added sample data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner2')
 
@@ -6,3 +7,6 @@ streamlit.header('💀Breakfast Menu')
 streamlit.text('🎉Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥭Kale, Spnach & Rocket Smoothie')
 streamlit.text('🍋Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
