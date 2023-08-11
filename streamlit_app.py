b73746755de0366c78a45742b1757d73471daaae
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:30:57 +0200

    link correection

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruitiyvice.com/api/fruit" +fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # streamlit.dataframe(fruityvice_response)
