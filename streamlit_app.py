bce1ea289dc2f9478108638e414b06822194ae9f
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:14:20 +0200

    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get(
     "https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
