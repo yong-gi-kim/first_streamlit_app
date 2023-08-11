c2ea226c2a9b3112a87626963641ac485b962543
yong-gi-kim <powergambler@gmail.com>
2023-08-11 13:27:06 +0200

    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ streamlit.text('🎉Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥭Kale, Spnach & Rocket Smoothie')
 streamlit.text('🍋Hard-Boiled Free-Range Egg')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
