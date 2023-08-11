b0f358489865bf12cef38e824cd7f8cbf2494637
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:10:59 +0200

    imported fruityvice & sample data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner2')
 
@@ -21,3 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
