38c8c57ed17b90796b85194da32d43a86cbaff4d
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:13:52 +0200

    format & display sample json

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,21 @@ streamlit.text('🎉Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥭Kale, Spnach & Rocket Smoothie')
 streamlit.text('🍋Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(
+    my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get(
+    "https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json)
