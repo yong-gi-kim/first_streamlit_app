c6dbde4b7f8d3e1c0cd059147c8fcb3d481c107e
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:18:49 +0200

    removed json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get(
     "https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 # write your own comment -what does the next line do? I will get the json displayed as "Pandas Table"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
