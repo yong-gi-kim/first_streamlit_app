c92ff3894154e0daed8ff24e780160b40008c965
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:35:36 +0200

    view Correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # streamlit.dataframe(fruityvice_response)
-streamlit.dataframe(streamlit.text(fruityvice_response.json()))
+normalizedSelectedFruit = streamlit.text(fruityvice_response.json())
+
+streamlit.dataframe(normalizedSelectedFruit)
