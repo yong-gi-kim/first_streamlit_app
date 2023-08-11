8527901c4751ca10d0cfac9a165e05b45c281902
yong-gi-kim <powergambler@gmail.com>
2023-08-11 16:19:54 +0200

    added input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,6 @@ fruityvice_response = requests.get(
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? -> in a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
