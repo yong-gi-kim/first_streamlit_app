8fd4b3cf6d37ec15c445d4f2c639810897d063d2
yong-gi-kim <powergambler@gmail.com>
2023-08-14 10:22:31 +0200

    nexted in if-else

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,16 @@ streamlit.header("Fruityvice Fruit Advice!")
 # # write your own comment - what does this do? -> in a dataframe
 # streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-# streamlit.dataframe(fruityvice_response)
-normalizedSelectedFruit = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(normalizedSelectedFruit)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.write('The user entered ', fruit_choice)
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+        normalizedSelectedFruit = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(normalizedSelectedFruit)
+except URLError as e:
+    streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
